781debde85d6380e2b1e06b5c22e985dcfa31238
Ankitsgit <ankit77us@gmail.com>
2025-02-16 23:28:02 +0530

    null comparision with 0

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,27 @@
 // console.log(2 > 1);
 // console.log(2 >= 1);
 // console.log(2 < 1);
-// console.log(2 == 1);
+// console.log(2 == 1);=> false
 // console.log(2 != 1);
+// console.log(2 === 1);==> false
+// === is strict equality operator and == is loose equality operator
+// example of loose equality operator
+// console.log(2 == "2");=> true
+// console.log(2 === "2");=> false
 
+// when data type is different, then js convert one data type to another data type
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);==> true because "2" is converted to 2
+// console.log("02" > 1);==> false because "02" is converted to 2
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);// ==> false because null is converted to 0
+console.log(null == 0);// ==> false because null is converted to 0
+console.log(null >= 0);// ==> true because null is converted to 0 
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);// ==> false because undefined is converted to NaN
+console.log(undefined > 0);// ==> false
+console.log(undefined < 0);// ==> false
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2);// ==> false
