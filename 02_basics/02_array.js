d8fc56180cf94957f487b35d33a1ad14a4c6e3b8
Ankitsgit <ankit77us@gmail.com>
2025-02-18 00:33:33 +0530

    Arrays in javascript

---
+++ 02_basics/02_array.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)//=> it will add the dc_heros array as a single element in the marvel_heros array
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);//=> f- it will return the 2nd character of the 4th element
+
+// const allHeros = marvel_heros.concat(dc_heros)//=> it will merge the two arrays
+// console.log(allHeros);//=> ["thor", "Ironman", "spiderman", "superman", "flash", "batman"]
+
+const all_new_heros = [...marvel_heros, ...dc_heros]//=> it will merge the two arrays
+
+// console.log(all_new_heros);//=> ["thor", "Ironman", "spiderman", "superman", "flash", "batman"]
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)//=> it will flatten the array
+console.log(real_another_array);//=> [1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
+
+
+
+console.log(Array.isArray("Ankit"));//=> false
+console.log(Array.from("Ankit"));//=> ["A", "n", "k", "i", "t"]
+console.log(Array.from({name: "Ankit"})) // => []
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));//=> [100, 200, 300]
