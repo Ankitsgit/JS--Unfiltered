8461bc7775aa262e63267b2bc5cc12bd3fca0660
Ankitsgit <ankit77us@gmail.com>
2025-03-08 16:33:09 +0530

    inheritance in js

---
+++ 10_classes_and_oops/07_inheritance.js
@@ -0,0 +1,31 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{//Teacher is a subclass of User
+    constructor(username, email, password){
+        super(username)//super is used to call the constructor of parent class
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);//true
+console.log(chai instanceof Teacher);//true
